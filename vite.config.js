5268e9b7befad75621e38bef488267b45d857e65
aleksey-09 <Zigmenb280@gmail.com>
2026-01-13 19:09:35 +0200

    Правильный base с слешем

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/HouseStaff-react/',
+  base: '/HouseStaff-react-/',
   plugins: [react()],
 })
